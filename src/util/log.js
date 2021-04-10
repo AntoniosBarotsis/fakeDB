6d1f8d40593f354e0ff4aeb6c69c120a6a3845e4
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2021-04-11 01:14:51 +0200

    Made stuff work with arrays

--- src/util/log.js
+++ src/util/log.js
@@ -1,15 +1,15 @@
 module.exports = { log, warning, error, mapped }
 
-function log(str) {
+function log(...str) {
     console.log("\033[32m[ LOG ]    \033[0m %s", str)
 }
 
-function warning(str) {
+function warning(...str) {
     console.log("\033[33m[ WARNING ]\033[0m %s", str) 
 }
 
-function error(str) {
-    console.log("\033[31m[ ROOM ]   \033[0m %s", str)
+function error(...str) {
+    console.log("\033[31m[ ERROR ]   \033[0m", str)
 }
 
 function mapped(str) {
